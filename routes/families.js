aa08dfad585b7bb209b1873325151755b32646fc
“Vilde <vilde@vevatne.no>
2017-05-24 21:11:30 -0700

    Add post route for family

--- routes/families.js
+++ routes/families.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+const mongoose = require('./../db/mongoose'); 
+const {Family} = require('./../models/family'); 
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/new', function(req, res, next) {
+  res.render('families/new');
 });
+
+router.post('/', (req, res, next) => {
+	console.log("made it here"); 
+	var family = new Family(req.body)
+
+	family.save().then(() => {
+		res.render('families/show', {family})
+	}).catch((e) => console.log(e))
+})
 
 module.exports = router;
