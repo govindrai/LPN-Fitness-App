1f3a3f1aa15137458723f9afd839477d3285772c
“Vilde <vilde@vevatne.no>
2017-05-25 18:23:46 -0700

    Adds new view for users

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+const mongoose = require('./../db/mongoose'); 
+const {User} = require('./../models/user'); 
+const {Family} = require('./../models/family');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/new', function(req, res, next) {
+Family.find({}).then((families) => {
+		User.find({}).populate('family').then((users) => {
+			res.render('users/new', {families, users});
+		})
+	}); 
 });
+
+router.post('/', (req, res, next) => {
+	console.log("made it here"); 
+	var user = new User(req.body)
+	console.log(user)
+
+	user.save().then((user) => {
+		res.redirect('/users/new')
+	}).catch((e) => console.log(e))
+})
 
 module.exports = router;
