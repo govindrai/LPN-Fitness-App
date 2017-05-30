161216c1a64e5ad71b65002865c963564fb0feee
“Vilde <vilde@vevatne.no>
2017-05-29 22:43:20 -0700

    Add user and challenge ids to participation model

--- models/participation.js
+++ models/participation.js
@@ -1,2 +1,20 @@
 const mongoose = require('mongoose');
-var Schema = mongoose.Schema; 
+
+const User = require('./../models/user');
+const Challenge = require('./../models/challenge');
+var Schema = mongoose.Schema;
+
+var participationSchema = new Schema({
+  challengeId: {
+    type: [{Schema.Types.ObjectId, ref: 'Challenge'}],
+    required: true
+  },
+    userId: {
+      type: [{Schema.Types.ObjectId, ref 'User'}],
+      required: true
+    }
+});
+
+var Participation = mongoose.model('Participation', participationSchema);
+
+module.exports = Participation;
