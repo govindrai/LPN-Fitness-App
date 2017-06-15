700ca98eff5bedafeb910d78155c89236f84128d
“Vilde <vilde@vevatne.no>
2017-06-15 12:58:12 -0700

    Remove nickname and family name listing from accounts index

--- routes/accounts.js
+++ routes/accounts.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var router = express.Router();
 const mongoose = require('./../db/mongoose'); 
-const {User} = require('./../models/user'); 
+const User = require('./../models/user'); 
 
 /* GET users listing. */
 
 router.get('/', function(req, res, next) {
 	User.getAdmins().then((admins) => {
 		User.getNonAdmins().then((nonAdmins) => {
-			res.render('settings/index', {admins, nonAdmins});
+			res.render('accounts/index', {admins, nonAdmins});
 		})
 	})
 })
