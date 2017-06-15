c801ea3b2f5be72156e5fcee950ee075a1954f9a
“Vilde <vilde@vevatne.no>
2017-06-15 14:15:12 -0700

    Reroutes changeDmin to users routes

--- routes/users.js
+++ routes/users.js
@@ -23,18 +23,28 @@ router.post('/', (req, res, next) => {
 				res.redirect('/families');
 		});
 	}).catch(e => console.log(err));
-})
+});
 
 // initialize new user, if save successful,
 // generate auth token and take to home page
 router.post('/', (req, res, next) => {
-	var user = new User(req.body)
+	var user = new User(req.body);
 
 	user.save().then((user) => {
 		user.generateAuthToken().then((token) => {
 				res.redirect('/families/');
 		});
 	}).catch(e => console.log(err));
-})
+});
+
+router.put('/edit', (req, res) => {
+	console.log(req.body);
+	console.log(req.body.user);
+	User.findById(req.body.user)
+	.then(user => {
+		console.log(user);
+	})
+	.catch(e => console.log(e));
+});
 
 module.exports = router;
