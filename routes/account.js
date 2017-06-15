8987ae81572831e15e66714acebbf2999c0195b6
“Vilde <vilde@vevatne.no>
2017-06-15 16:04:35 -0700

    Add middleware to check if admin

--- routes/account.js
+++ routes/account.js
@@ -5,16 +5,24 @@ const User = require('./../models/user');
 
 /* GET users listing. */
 
+function isAdmin(req, res, next){
+	if (res.locals.user.admin) {
+		next();
+	} else {
+    res.render('sessions/unauthorized', {message: "Only Admins have access to admin-settings you fuck"});
+  }
+}
+
 router.get('/', function(req, res, next) {
 	User.findById(res.locals.user._id).then((user) => {
 		res.render('users/edit', {user});
 	});
 });
 
-router.get('/admin-settings', function(req, res, next) {
+router.get('/admin-settings', isAdmin , function(req, res, next) {
 	User.getAdmins().then((admins) => {
 		User.getNonAdmins().then((nonAdmins) => {
-			res.render('account/index', {admins, nonAdmins});
+			res.render('account/admin_settings', {admins, nonAdmins});
 		});
 	});
 });
@@ -22,7 +30,7 @@ router.get('/admin-settings', function(req, res, next) {
 router.put('/profile', function(req, res, next) {
 		User.findOneAndUpdate({_id: res.locals.user._id}, {$set: req.body})
 		.then(user => {
-			res.render('account/index', {user});
+			res.render('users/edit', {user});
 		}).catch(e => console.log(e)); 
 });
 
