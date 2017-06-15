c923341905bdcabfb1dcc59661880eead33890d7
“Vilde <vilde@vevatne.no>
2017-06-15 15:52:41 -0700

    Make edit profile page index

--- routes/account.js
+++ routes/account.js
@@ -6,6 +6,12 @@ const User = require('./../models/user');
 /* GET users listing. */
 
 router.get('/', function(req, res, next) {
+	User.findById(res.locals.user._id).then((user) => {
+		res.render('users/edit', {user});
+	});
+});
+
+router.get('/admin-settings', function(req, res, next) {
 	User.getAdmins().then((admins) => {
 		User.getNonAdmins().then((nonAdmins) => {
 			res.render('account/index', {admins, nonAdmins});
@@ -13,16 +19,10 @@ router.get('/', function(req, res, next) {
 	});
 });
 
-router.get('/profile', function(req, res, next) {
-	User.findById(res.locals.user._id).then((user) => {
-		res.render('users/edit', {user}); 
-	}).catch(e => console.log(e)); 
-}); 
-
 router.put('/profile', function(req, res, next) {
 		User.findOneAndUpdate({_id: res.locals.user._id}, {$set: req.body})
 		.then(user => {
-			res.render('users/edit', {user});
+			res.render('account/index', {user});
 		}).catch(e => console.log(e)); 
 });
 
