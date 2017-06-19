02233bd0fc8b65da44a36425fbafd3adb0d5d1ec
Govind Rai <raigovind93@gmail.com>
2017-06-19 11:41:00 -0700

    Merge updating user info into users/edit

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,11 @@
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/lpn');
+mongoose.connect('mongodb://localhost:27017/lpn', (error) => {
+  if (error) console.log("There was an error: ", error);
+  console.log("Successfully connected to LPN");
+  console.log(mongoose.modelNames());
+});
+
 mongoose.connection.on('error', () => {
   console.log("Mongoose Collection Error!");
 });
