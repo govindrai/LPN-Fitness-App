0c7a885117f19d9f6924e0d92bfc3aa67bf66bc1
Govind Rai <raigovind93@gmail.com>
2017-06-21 14:17:57 -0700

    Add code for easier debugging in generateSchedule()

--- models/challenge.js
+++ models/challenge.js
@@ -66,66 +66,11 @@ challengeSchema.statics.getAllExceptPastChallengesCount = () => {
 	return Challenge.find().where('date.end').gt(new Date()).count();
 };
 
-// challengeSchema.statics.getFamilyIds = () => {
-// 	return Family.find({}).select('_id');
-// };
-
-// challengeSchema.methods.generateSchedule = () => {
-// 	return Challenge.getFamilyIds()
-// 	.then(aggregationObjs => {
-// 		familyIds = aggregationObjs.map(aggregationObj => {
-// 			return aggregationObj._id;
-// 		});
-// 		familyIds.push("Bye");
-// 		var schedule = {
-// 	    week1: {},
-// 	    week2: {},
-// 	    week3: {},
-// 	    week4: {},
-// 	    week5: {},
-// 	    week6: {},
-// 	    week7: {}
-// 	  };
-
-// 	  familyIds.forEach((family, index) => {
-// 	    var newFamilies = familyIds.filter(newFamily => newFamily != family);
-// 	    var week = 1;
-// 	    for (var i = 0; i < 7; i++) {
-// 	      var weekNumber = "week" + week;
-// 	      if (schedule[weekNumber][family]) {
-// 	      	// don't do anything since fam not free that week
-// 	      } else if (schedule[weekNumber][newFamilies[i]]) {
-// 	      	var tempFams = newFamilies.slice(i+1);
-// 	    		tempFams.some(tempFam => {
-// 	    			if(schedule[weekNumber][tempFam]) {
-// 	    				return false;
-// 	    				// do nothing as fam is unavailable
-// 	    			} else {
-// 	    				 // add a verse for opposing family
-// 			        schedule[weekNumber][family] = tempFam;
-// 			        // add same entry for versing family
-// 			        schedule[weekNumber][tempFam] = family;
-// 			        newFamilies[newFamilies.indexOf(tempFam)] = newFamilies[i];
-// 							return true;
-// 	    			}
-// 	    		});
-// 	      } else {
-// 	        // add a verse for opposing family
-// 	        schedule[weekNumber][family] = newFamilies[i];
-// 	        // add same entry for versing family
-// 	        schedule[weekNumber][newFamilies[i]] = family;
-// 	      }
-//       week++;
-// 	    }
-// 	  });
-// 	  return schedule;
-// 	});
-// };
-
 challengeSchema.methods.generateSchedule = () => {
 	return Family.find()
 	.then(families => {
-		families = families.map(family => family.toObject());
+		// use below line to better debug
+		// families = families.map(family => family.toObject());
 		var schedule = {
 	    week1: {},
 	    week2: {},
@@ -135,7 +80,6 @@ challengeSchema.methods.generateSchedule = () => {
 	    week6: {},
 	    week7: {}
 	  };
-	  debugger;
 	  families.forEach((family, index) => {
 	    var newFamilies = families.filter(newFamily => newFamily.name != family.name);
 	    var week = 1;
