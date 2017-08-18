56754c77b62cb29b8b47ff004466e9516522d2ce
Govind Rai <raigovind93@gmail.com>
2017-08-17 17:19:31 -0700

    store color defaults inside res.locals to avoid littering views

--- middleware/verifyAuthorization.js
+++ middleware/verifyAuthorization.js
@@ -26,6 +26,21 @@ function verifyAuthorization(req, res, next) {
       User.decodeAuthorizationToken(res.locals.token)
         .then(user => {
           if (!user) return res.status(404).send("UNAUTHORIZED.");
+          res.locals.familyColors = {
+            Topaz: "#f2cd46",
+            Sunstone: "#faa152",
+            Ruby: "#f83b54",
+            Alexandrite: "#cb51d7",
+            Iolite: "#6456f8",
+            Sapphire: "#3b8df8",
+            Emerald: "#24ab60",
+            Bye: "brown"
+          };
+          res.locals.statusColors = {
+            winning: "#0b8c0b",
+            losing: "#d92b3a",
+            tied: "brown"
+          };
           res.locals.home = "/families/" + user.family.name;
           if (req.path == "/") return res.redirect(res.locals.home);
           res.locals.loggedIn = true; //actually don't need this; can just check for user
