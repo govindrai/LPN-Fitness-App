2e3c78d3e1607e99a3a0aee9e4394aa3a0291e8d
Govind Rai <raigovind93@gmail.com>
2017-08-29 19:37:02 -0700

    Fix logic and styling for Challenges#Index

--- routes/challenges.js
+++ routes/challenges.js
@@ -1,62 +1,70 @@
 // Modules
-const express = require('express'),
-  _ = require('lodash');
+const express = require("express"),
+  _ = require("lodash");
 
 // Models
-const Challenge = require('./../models/challenge'),
-  Family = require('./../models/family'),
-  Participation = require('./../models/participation');
+const Challenge = require("./../models/challenge"),
+  Family = require("./../models/family"),
+  Participation = require("./../models/participation");
 
 // Middleware
-const isAdmin = require('./../middleware/isAdmin');
+const isAdmin = require("./../middleware/isAdmin");
 
 const router = express.Router();
 
 // GET list all challenges
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   var currentChallenge, futureChallenges, pastChallenges;
-  if (res.locals.loggedIn) {
-    Challenge.getCurrentChallenge()
-      .then(challenge => {
-        currentChallenge = challenge;
-        return Participation.setUserParticipationForChallenges(res.locals.user, [currentChallenge]);
-      })
-      .then(() => {
-        return Challenge.getFutureChallenges();
-      })
-      .then(challenges => {
-        futureChallenges = challenges;
-        return Participation.setUserParticipationForChallenges(res.locals.user, futureChallenges);
-      })
-      .then(() => {
-        return Challenge.getPastChallenges();
-      })
-      .then(challenges => {
-        pastChallenges = challenges;
-        return Participation.setUserParticipationForChallenges(res.locals.user, pastChallenges);
-      })
-      .then(() => {
-        res.render('challenges/index', { currentChallenge, futureChallenges, pastChallenges });
-      })
-      .catch(e => console.log(e));
-  } else {
-    res.render('sessions/unauthorized');
-  }
+  Challenge.getCurrentChallenge()
+    .then(challenge => {
+      currentChallenge = challenge;
+      return Participation.setUserParticipationForChallenges(res.locals.user, [
+        currentChallenge
+      ]);
+    })
+    .then(() => {
+      return Challenge.getFutureChallenges();
+    })
+    .then(challenges => {
+      futureChallenges = challenges;
+      return Participation.setUserParticipationForChallenges(
+        res.locals.user,
+        futureChallenges
+      );
+    })
+    .then(() => {
+      return Challenge.getPastChallenges();
+    })
+    .then(challenges => {
+      pastChallenges = challenges;
+      return Participation.setUserParticipationForChallenges(
+        res.locals.user,
+        pastChallenges
+      );
+    })
+    .then(() => {
+      res.render("challenges/index", {
+        currentChallenge,
+        futureChallenges,
+        pastChallenges
+      });
+    })
+    .catch(e => console.log(e));
 });
 
 // Create Challenge Form
-router.get('/new', isAdmin, (req, res) => {
-  res.render('challenges/new');
+router.get("/new", isAdmin, (req, res) => {
+  res.render("challenges/new");
 });
 
 // Create Challenge
-router.post('/', (req, res) => {
-  var body = _.pick(req.body, ['name', 'date.start', 'date.end']);
+router.post("/", (req, res) => {
+  var body = _.pick(req.body, ["name", "date.start", "date.end"]);
   var challenge = new Challenge(body);
   challenge
     .save()
     .then(() => {
-      res.redirect('/challenges');
+      res.redirect("/challenges");
     })
     .catch(e => console.log(e));
 });
