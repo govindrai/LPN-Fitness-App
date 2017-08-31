5d2878b53efd5fc7044e06a81c7b97db05bd0fe3
Govind Rai <raigovind93@gmail.com>
2017-08-30 23:25:05 -0700

    Add restructured add points entry skeleton and further style add points modal

--- routes/activities.js
+++ routes/activities.js
@@ -1,18 +1,18 @@
 // Modules
-const express = require('express'),
-  pug = require('pug');
+const express = require("express"),
+  pug = require("pug");
 
 // Models
-const Activity = require('./../models/activity'),
-  Unit = require('./../models/unit');
+const Activity = require("./../models/activity"),
+  Unit = require("./../models/unit");
 
 // Middleware
-const isAdmin = require('./../middleware/isAdmin');
+const isAdmin = require("./../middleware/isAdmin");
 
 const router = express.Router();
 
 // GET list all activities
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   if (req.xhr) {
     Activity.find({})
       .then(activities => {
@@ -25,46 +25,51 @@ router.get('/', (req, res) => {
       .catch(e => console.log(e));
   } else {
     Unit.find({}).then(units => {
-      Activity.find({}).populate('unit').then(activities => {
-        res.render('activities/index', { activities });
+      Activity.find({}).populate("unit").then(activities => {
+        res.render("activities/index", { activities });
       });
     });
   }
 });
 
 // GET new activity form
-router.get('/new', isAdmin, (req, res) => {
+router.get("/new", isAdmin, (req, res) => {
   Unit.find({}).then(units => {
-    Activity.find({}).populate('unit').then(activities => {
+    Activity.find({}).populate("unit").then(activities => {
       console.log(activities);
-      res.render('activities/new', { units, activities });
+      res.render("activities/new", { units, activities });
     });
   });
 });
 
 // GET activity info
-router.get('/:activityName', (req, res) => {
+router.get("/:activityName", (req, res) => {
   if (req.xhr) {
     Activity.findOne({ name: req.params.activityName })
-      .populate('unit')
+      .populate("unit")
       .then(activity => {
-        var file = req.query.partial == 'false' ? '_form.pug' : '_activity.pug';
-        res.send(pug.renderFile(process.env.PWD + '/views/points/' + file, { activity, date: new Date() }));
+        res.send(
+          pug.renderFile(
+            process.env.PWD + "/views/points/" + "_point_entry.pug",
+            {
+              activity,
+              date: new Date()
+            }
+          )
+        );
       })
-      .catch(e => {
-        res.status(404).send('No Activity with name ' + req.params.activityName);
-      });
+      .catch(e => console.log(e));
   } else {
-    res.status(400).send('Not an XHR request');
+    res.status(400).send("Not an XHR request");
   }
 });
 
-router.post('/', (req, res, next) => {
+router.post("/", (req, res, next) => {
   var activity = new Activity(req.body);
   activity
     .save()
     .then(activity => {
-      res.redirect('/activities/new');
+      res.redirect("/activities/new");
     })
     .catch(e => console.log(e));
 });
