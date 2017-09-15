0302d7fb5bf0032248cce2335c2332336ecc6cea
“Vilde <vilde@vevatne.no>
2017-09-14 18:00:08 -0700

    Add point decrement to users total points upon deletion

--- routes/challenges.js
+++ routes/challenges.js
@@ -6,6 +6,7 @@ const express = require("express"),
 const Challenge = require("./../models/challenge"),
   Family = require("./../models/family"),
   Participation = require("./../models/participation"),
+  User = require("./../models/user"),
   Point = require("./../models/point");
 
 // Middleware
@@ -104,23 +105,51 @@ router.get("/:id/edit", isAdmin, (req, res) => {
 
 // Delete challenge
 router.delete("/:id", (req, res) => {
-  let participationIds = [];
+  let participations;
+  let users = [];
+  let points = [];
   Challenge.remove({ _id: req.params.id })
     .then(() => {
       return Participation.find({ challenge: req.params.id });
     })
-    .then(participations => {
-      participations.forEach(participation =>
-        participationIds.push(participation._id)
-      );
+    .then(participationsArray => {
+      participations = participationsArray;
       return Participation.remove({ challenge: req.params.id });
     })
     .then(() => {
-      return Point.remove({ _id: { $in: participationIds } });
+      return Promise.all(
+        participations.map(participation => Point.find({ participation }))
+      );
+    })
+    .then(pointsArraysArray => {
+      const pointsToDecrementArray = [];
+      pointsArraysArray.forEach(pointsArray => {
+        pointsToDecrementArray.push(
+          pointsArray.reduce((total, point) => {
+            return (total += point.calculatedPoints);
+          }, 0)
+        );
+      });
+      console.log("Still ahve access to participations?", participations);
+      const decrementLifeTimePointPromises = participations.map(
+        (participation, index) => {
+          return User.findById(participation.user).then(user => {
+            user.update({
+              $inc: { lifetimePoints: pointsToDecrementArray[index] * -1 }
+            });
+          });
+        }
+      );
+
+      return Promise.all(decrementLifeTimePointPromises);
     })
     .then(() => {
-      return res.redirect("/challenges");
+      const participationIds = participations.map(
+        participation => participation._id
+      );
+      return Point.remove({ _id: { $in: participationIds } });
     })
+    .then(() => res.redirect("/challenges"))
     .catch(e => console.log(e));
 });
 
