39b2a865eb6cee873b5cef037c28e29b9ffa58e6
“Vilde <vilde@vevatne.no>
2018-11-23 09:00:34 -0800

    Updates with json files

--- config/db/seed.js
+++ config/db/seed.js
@@ -54,93 +54,6 @@ let iolite,
   lastYearChallenge,
   twoYearsAgoChallenge;
 
-units = [
-  {
-    name: "Mile",
-    abbreviation: "mi"
-  },
-  {
-    name: "Game",
-    abbreviation: "game"
-  },
-  {
-    name: "Feet",
-    abbreviation: "ft"
-  },
-  {
-    name: "Meter",
-    abbreviation: "mm"
-  },
-  {
-    name: "Minute",
-    abbreviation: "min"
-  },
-  {
-    name: "Hour",
-    abbreviation: "hr"
-  },
-  {
-    name: "Hole",
-    abbreviation: "hole"
-  },
-  {
-    name: "Day",
-    abbreviation: "day"
-  },
-  {
-    name: "Jump",
-    abbreviation: "jump"
-  }
-];
-
-families = [
-  {
-    name: "Iolite",
-    motto: "The best family",
-    challengesWon: 3,
-    playoffsReached: 4
-  },
-  {
-    name: "Alexandrite",
-    motto: "The 2nd best family",
-    challengesWon: 4,
-    playoffsReached: 3
-  },
-  {
-    name: "Sunstone",
-    motto: "A family in LPN",
-    challengesWon: 5,
-    playoffsReached: 2
-  },
-  {
-    name: "Ruby",
-    motto: "Another family in LPN",
-    challengesWon: 3,
-    playoffsReached: 1
-  },
-  {
-    name: "Sapphire",
-    motto: "Yet another family in LPN",
-    challengesWon: 2,
-    playoffsReached: 1
-  },
-  {
-    name: "Topaz",
-    motto: "A chill family in LPN",
-    challengesWon: 2,
-    playoffsReached: 2
-  },
-  {
-    name: "Emerald",
-    motto: "Almost forgot this family",
-    challengesWon: 1,
-    playoffsReached: 2
-  },
-  {
-    name: "Bye"
-  }
-];
-
 // generates an array of dates for two challenges in the past,
 // two in the future and one in the present
 // SEED FILE ONLY FUNCTION
@@ -406,9 +319,9 @@ async function createParticipations() {
 }
 
 async function seedGlobalData() {
-  await Family.create(families);
+  await Family.create(require("./families.json"));
   await Activity.create(require("./activities.json"));
-  await Unit.create(units);
+  await Unit.create(require("./units.json"));
   await assignUnits();
 }
 
