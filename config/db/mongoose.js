c8cfa505075e4027b7bc2a58403f2851659432c7
Govind Rai <raigovind93@gmail.com>
2019-01-30 08:13:10 -0800

    Add nodemon to repository

--- config/db/mongoose.js
+++ config/db/mongoose.js
@@ -1,15 +1,18 @@
-const mongoose = require("mongoose"),
-  { MONGO_URL } = require("../keys.js");
+const mongoose = require('mongoose'),
+  { MONGO_URL } = require('../keys.js');
 
 mongoose.Promise = global.Promise;
-mongoose.set("debug", true);
+mongoose.set('debug', true);
 
 mongoose
-  .connect(MONGO_URL, { useMongoClient: true })
+  .connect(
+    MONGO_URL,
+    { useNewUrlParser: true }
+  )
   .then(() => {
-    console.log("Successfully connected to LPN");
-    console.log("Model Names: ", mongoose.modelNames());
+    console.log('Successfully connected to LPN');
+    console.log('Model Names: ', mongoose.modelNames());
   })
-  .catch(e => console.log("There was an error: ", e));
+  .catch(e => console.log('There was an error: ', e));
 
 module.exports = mongoose;
