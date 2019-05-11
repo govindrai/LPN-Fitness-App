0d12173ce1f796610cea728a0412d81290a9140c
Govind Rai <raigovind93@gmail.com>
2019-05-11 11:42:21 -0700

    PR-94 Update keys.js to let dev know about missing devkeys.js file

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,21 @@
-module.exports =
-  process.env.NODE_ENV === "production"
-    ? require("./prodkeys")
-    : require("./devkeys");
+let keys;
+
+/* eslint-disable global-require */
+if (process.env.NODE_ENV === 'production') {
+  keys = require('./prodkeys');
+} else {
+  try {
+    keys = require('./devkeys');
+  } catch (e) {
+    if (e.message === "Cannot find module './devkeys'") {
+      const err = new Error('First time using the repo? You need to create a devkeys.js file in /config. The contents should match prodkeys.js, replaced with your personal keys.');
+      err.name = 'MissingKeyFile';
+      throw err;
+    }
+    throw e;
+  }
+}
+/* eslint-enable global-require */
+
+
+module.exports = keys;
